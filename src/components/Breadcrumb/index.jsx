4c5761237abb845d852df1d58383e9e95715bc74
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-02-15 20:35:30 -0300

    Add Componente Breadcrumb

---
+++ src/components/Breadcrumb/index.jsx
@@ -0,0 +1,32 @@
+import { Link, useLocation } from 'react-router-dom';
+import styles from './Breadcrumb.module.css';
+
+export default function Breadcrumb() {
+    const location = useLocation();
+
+    let currentLink = '';
+
+    const crumbs = location.pathname.split('/').filter(crumb => crumb !== '');
+
+    function firstLetterUppercase(phrase) {
+        return phrase.charAt(0).toUpperCase() + phrase.slice(1);
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={`${styles.wrapper} container`}>
+                {crumbs.map(crumb => {
+                    currentLink += `/${crumb}`;
+
+                    return (
+                        <div className={`${styles.crumb}`} key={crumb}>
+                            <Link to={currentLink}>
+                                {firstLetterUppercase(crumb)}
+                            </Link>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
