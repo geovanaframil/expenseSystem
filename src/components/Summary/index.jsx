7cb30b61dbfb4bc6e2fbe08cc7055fabdd15635e
Geovana Framil <geovanaframil@hotmail.com>
2023-02-16 20:29:46 -0300

    Adiciona função para formatar moeda

--- src/components/Summary/index.jsx
+++ src/components/Summary/index.jsx
@@ -1,16 +1,32 @@
+import { formatPrice } from "../../utils";
 import styles from "./Summary.module.css";
+
+const example = {
+  id: "exp1",
+  name: "Despesa 1",
+  categoryID: "cat1",
+  userID: "user_1abc2",
+  amount: 1000,
+  status: "PENDENTE",
+  _user: {
+    id: "user_1abc2",
+    name: "John",
+    lastName: "Doe",
+    email: "johndoe@example.com",
+  },
+};
 
 function Summary() {
   return (
     <div className={styles.summary}>
       <div className={styles.totalPaid}>
         <p>TOTAL PAGO</p>
-        <p>Teste</p>
+        <p>{formatPrice(example.amount)}</p>
         <hr className={styles.green}></hr>
       </div>
       <div className={styles.totalExpenses}>
         <p>TOTAL DESPESAS</p>
-        <p>Teste</p>
+        <p>{formatPrice(example.amount)}</p>
         <hr className={styles.red}></hr>
       </div>
     </div>
