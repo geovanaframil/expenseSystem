c48873e8a2f3bfd93d7aa275e431260b0c36ceb6
Geovana Framil <geovanaframil@hotmail.com>
2023-02-16 21:29:52 -0300

    Função handleInputChange

--- src/components/Filters/Search.jsx
+++ src/components/Filters/Search.jsx
@@ -1,10 +1,18 @@
 import styles from "./Filters.module.css";
 
 function Search() {
+  const handleInputChange = () => {
+    console.log("teste");
+  };
+
   return (
     <div className={styles.search}>
       <label>Buscar</label>
-      <input className={styles.inputSearch} type="text" />
+      <input
+        className={styles.inputSearch}
+        type="text"
+        onChange={handleInputChange}
+      />
     </div>
   );
 }
