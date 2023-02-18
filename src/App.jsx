eef4412b478a65f523771feec7b1ced5517a0af4
Geovana Framil <geovanaframil@hotmail.com>
2023-02-18 07:02:34 -0300

    Filtro por id

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
             <Header />
             <Breadcrumb/>
             <Router />
+            <Search />
         </div>
     );
 }
--- src/components/Filters/Search.jsx
+++ src/components/Filters/Search.jsx
@@ -1,9 +1,55 @@
+import { useState } from "react";
 import styles from "./Filters.module.css";
 
 function Search() {
-  const handleInputChange = () => {
-    console.log("teste");
+  const [expenses, setExpenses] = useState([
+    {
+      id: "exp1",
+      name: "Despesa 1",
+      categoryID: "cat1",
+      userID: "user_1abc2",
+      amount: 1000,
+      status: "PENDENTE",
+      _user: {
+        id: "user_1abc2",
+        name: "John",
+        lastName: "Doe",
+        email: "johndoe@example.com",
+      },
+      _category: {
+        id: "cat1",
+        name: "Alimentação",
+      },
+    },
+    {
+      id: "exp2",
+      name: "Despesa 2",
+      categoryID: "cat2",
+      userID: "user_1abc2",
+      amount: 2000,
+      status: "PAGO",
+      _user: {
+        id: "user_1abc2",
+        name: "John",
+        lastName: "Doe",
+        email: "johndoe@example.com",
+      },
+      _category: {
+        id: "cat2",
+        name: "Transporte",
+      },
+    },
+  ]);
+
+  const [filterValue, setFilterValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setFilterValue(event.target.value);
   };
+
+  const filteredExpenses = expenses.filter((expense) =>
+    expense.id.toString().includes(filterValue)
+  );
 
   return (
     <div className={styles.search}>
@@ -12,7 +58,15 @@ function Search() {
         className={styles.inputSearch}
         type="text"
         onChange={handleInputChange}
+        value={filterValue}
       />
+      <ul>
+        {filteredExpenses.map((expense) => (
+          <li key={expense.id}>
+            {expense.id} - {expense.name} - {expense.amount}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
