bdee371fb68f8a6fc5163b9659068b9161cdbe5d
Geovana Framil <geovanaframil@hotmail.com>
2023-02-23 20:59:41 -0300

    Função ordernar por funcionando com a api

--- src/components/Filters/OrderBy.jsx
+++ src/components/Filters/OrderBy.jsx
@@ -9,30 +9,50 @@ function OrderBy() {
   useEffect(() => {
     expensesAllUsers().then((data) => {
       let sortedUsers = [...data];
-
       console.log(sortedUsers)
-      // switch (order) {
-      //   case "user":
-      //     sortedUsers.sort((a, b) => a.name.localeCompare(b.name));
-      //     break;
-      //   case "expenses":
-      //     sortedUsers.sort((a, b) => a.email.localeCompare(b.email));
-      //     break;
-      //   case
-      // }
+
+      switch (order) {
+        case "user":
+          sortedUsers.sort((a, b) => a._user.name.localeCompare(b._user.name));
+          break;
+        case "expenses":
+          sortedUsers.sort((a, b) => a.amount - b.amount);
+          break;
+        case "status":
+          sortedUsers.sort((a, b) => a.status.localeCompare(b.status));
+          break;
+        default:
+          sortedUsers.sort((a, b) => a.id - b.id);
+          break;
+      }
+      setUsers(sortedUsers);
     });
-  }, []);
+  }, [order]);
+
+  const handleOrderByChange = (e) => {
+    setOrder(e.target.value);
+  };
 
   return (
     <div className={styles.orderBy}>
       <label>Ordernar Por:</label>
-      <select name="fieldOrder">
+      <select id="order" value={order} onChange={handleOrderByChange}>
         <option></option>
         <option value="id">Id</option>
         <option value="user">Usuário</option>
         <option value="expenses">Despesas</option>
         <option value="status">Status</option>
       </select>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <span>{user._user.id} - </span>
+            <span>{user._user.name} - </span>
+            <span>{user.amount} - </span>
+            <span>{user.status}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
