3c6af93b3c71e7404015fca1894552c23ef100ee
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-02-26 23:47:49 -0300

    Add componente tabela

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,35 @@
+import styles from './Table.module.css';
+
+export default function Table({ config, data }) {
+    return (
+        <div className="container">
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        {config.map(item => (
+                            <th key={item.label}>{item.label}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((usuario, index) => (
+                        <tr key={index} id={usuario.id}>
+                            {config.map(item => (
+                                <td
+                                    className={styles[item?.style]}
+                                    attr-key={item.key}
+                                    attr-value={usuario[item.key]}
+                                    onClick={() => {
+                                        item?.onClick && item.onClick(usuario);
+                                    }}
+                                >
+                                    {usuario?.action || usuario[item.key]}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
